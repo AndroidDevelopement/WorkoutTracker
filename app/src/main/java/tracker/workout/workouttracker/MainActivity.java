960629c70d0d5b39968e81d958b0c6b2af7ae13a
“kriscfoster”! <krisfoster@live.ie>
2017-10-19 19:23:57 +0100

    - Added Menu Buttons - OnClick button handlers takes you to a new activity - New Activities - CreateWorkoutActivity, LogWorkoutActivity, WorkoutDiaryActivity

--- app/src/main/java/tracker/workout/workouttracker/MainActivity.java
+++ app/src/main/java/tracker/workout/workouttracker/MainActivity.java
@@ -1,13 +1,50 @@
 package tracker.workout.workouttracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button createWorkoutButton;
+    public Button logWorkoutButton;
+    public Button workoutDiaryButton;
+
+    public void init() {
+        createWorkoutButton = (Button)findViewById(R.id.createWorkoutButton);
+        createWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createWorkout = new Intent(MainActivity.this, CreateWorkoutActivity.class);
+                startActivity(createWorkout);
+            }
+        });
+
+        logWorkoutButton = (Button)findViewById(R.id.logWorkoutButton);
+        logWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent logWorkout = new Intent(MainActivity.this, LogWorkoutActivity.class);
+                startActivity(logWorkout);
+            }
+        });
+
+        workoutDiaryButton = (Button)findViewById(R.id.workoutDiaryButton);
+        workoutDiaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent workoutDiary = new Intent(MainActivity.this, WorkoutDiaryActivity.class);
+                startActivity(workoutDiary);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 }
