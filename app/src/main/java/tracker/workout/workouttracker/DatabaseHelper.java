7948f1e483bda157274807ff7b3ef159827f0d81
15204377 <edgaras.lagunovas@ucdconnect.ie>
2017-11-09 02:06:04 +0000

    DB rework + layout issues fix activity_log_workout layout - list item under button, incorrect list size activity_workout_diary layout - list item under button warnings fix

--- app/src/main/java/tracker/workout/workouttracker/DatabaseHelper.java
+++ app/src/main/java/tracker/workout/workouttracker/DatabaseHelper.java
@@ -5,14 +5,16 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
     public static final String DATABASE_NAME = "workout_tracker.db";
-    public static final String EXERCISES_TABLE = "exercises";
-    public static final String WORKOUT_TABLE = "workouts";
-    public static String strSeparator = "__,__";
+	public static final String EXERCISE_TABLE = "exercise";
+	public static final String CATEGORY_TABLE = "category";
+	public static final String CATEGORY_EXERCISE_TABLE = "category_exercise";
+    public static final String WORKOUT_TABLE = "workout";
+    public static final String WORKOUT_EXERCISE_TABLE = "workout_exercise";
+
     private Context context;
 
     public DatabaseHelper(Context context) {
@@ -29,96 +31,117 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             db.execSQL(string);
         }
 
-        for (int i = 0; i < resExercises.length; i++) {
-            String[] string = resExercises[i].split(":");
-            String categoryName = resCategories[Integer.parseInt(string[0])];
-            String exerciseName = string[1];
-            ContentValues cv = new ContentValues();
-            cv.put("name", exerciseName);
-            cv.put("category", categoryName);
-            db.insert("exercises", null, cv);
-        }
+        // Store categories in to db
+        for (int i = 0; i < resCategories.length; i++) {
+        	ContentValues cv = new ContentValues();
+        	cv.put("name", resCategories[i]);
+        	db.insert(CATEGORY_TABLE, null, cv);
+		}
+
+		// Store exercises + category_exercise
+		for (int i = 0; i < resExercises.length; i++) {
+        	String[] exercise = resExercises[i].split(":");
+        	int categoryId = Integer.parseInt(exercise[0]);
+        	ContentValues cv = new ContentValues();
+        	cv.put("name", exercise[1]);
+        	db.insert(EXERCISE_TABLE, null, cv);
+
+        	cv.clear();
+
+        	cv.put("categoryId", categoryId);
+        	cv.put("exerciseId", i + 1); // AA starts from 1.
+			db.insert(CATEGORY_EXERCISE_TABLE, null, cv);
+		}
+
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + EXERCISES_TABLE);
-        db.execSQL("DROP TABLE IF EXISTS " + WORKOUT_TABLE);
-        onCreate(db);
-    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		reset(db, oldVersion, newVersion);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		reset(db, oldVersion, newVersion);
+	}
+
+	private void reset(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS " + WORKOUT_EXERCISE_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_EXERCISE_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS " + WORKOUT_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS " + EXERCISE_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE);
+		onCreate(db);
+	}
+
+    public long getExerciseId(String name) {
+		SQLiteDatabase db = getWritableDatabase();
+		Cursor c = db.query(EXERCISE_TABLE, new String[]{"id"}, "name = '" + name + "'", null, null, null, null);
+		if (c.getCount() > 0) {
+			c.moveToNext();
+			return c.getLong(0);
+		}
+		return -1;
+	}
 
     // Adds a new workout to the Database
     public void insertWorkout(String name, String[] exercises) {
-
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = getWritableDatabase();
         ContentValues cv = new ContentValues();
-        ArrayList<String> exercisesList = new ArrayList();
-        String[] workoutExercises;
 
-        for (int i = 0; i < exercises.length; i++) {
-            exercisesList.add(exercises[i].toString());
-        }
-
-        workoutExercises = exercisesList.toArray(new String[exercisesList.size()]);
+        // Insert workout
         cv.put("name", name);
-        cv.put("exercises", convertArrayToString(workoutExercises));
-        db.insert("workouts", null, cv);
+        long workoutId = db.insert(WORKOUT_TABLE, null, cv);
+
+        // Insert exercises of the workout
+		for (String exercise : exercises) {
+			cv.clear();
+			cv.put("workoutId", workoutId);
+			cv.put("exerciseId", getExerciseId(exercise));
+			db.insert(WORKOUT_EXERCISE_TABLE, null, cv);
+		}
     }
 
     // Returns a String array of all of the workout names
     public String[] getWorkoutNames() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor c = db.rawQuery("Select name from workouts", null);
-        List<String> e = new ArrayList<String>();
+        Cursor c = db.rawQuery("SELECT name FROM " + WORKOUT_TABLE, null);
+        String[] workouts = new String[c.getCount()];
 
-        while (c.moveToNext()) {
-            e.add(c.getString(0));
-        }
+        for (int i = 0; i < c.getCount(); i++) {
+        	c.moveToNext();
+        	workouts[i] = c.getString(0);
+		}
 
-        return e.toArray(new String[e.size()]);
+        return workouts;
     }
 
     // Returns a String array of all exercises associated with a workout
     public String[] getWorkoutExercises(String workoutName) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor c = db.rawQuery("Select exercises from workouts Where name like '" + workoutName + "'", null);
-        String[] ex = {};
+        Cursor c = db.rawQuery("SELECT e.name FROM " + WORKOUT_EXERCISE_TABLE + " we, " + WORKOUT_TABLE + " w, " + EXERCISE_TABLE + " e WHERE we.workoutId = w.id AND we.exerciseId = e.id AND w.name = '" + workoutName + "'", null);
+        String[] exercises = new String[c.getCount()];
 
-        while (c.moveToNext()) {
-            ex = (convertStringToArray(c.getString(0)));
-        }
+		for (int i = 0; i < c.getCount(); i++) {
+			c.moveToNext();
+			exercises[i] = c.getString(0);
+			System.out.println(exercises[i]);
+		}
 
-        return ex;
+        return exercises;
     }
 
     // Returns all exercises associated with a particular category
     public String[] getExercisesForCategory(String category) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor c = db.rawQuery("Select name from exercises Where category like '" + category + "'", null);
-        List<String> e = new ArrayList<String>();
+		SQLiteDatabase db = getWritableDatabase();
+        Cursor c = db.rawQuery("SELECT e.name FROM " + CATEGORY_EXERCISE_TABLE + " ce, " + EXERCISE_TABLE + " e, " + CATEGORY_TABLE + " c WHERE ce.categoryId = c.id AND c.name = '" + category + "' AND ce.exerciseId = e.id", null);
+        String[] exercises = new String[c.getCount()];
 
-        while (c.moveToNext()) {
-            e.add(c.getString(0));
-        }
+		for (int i = 0; i < c.getCount(); i++) {
+			c.moveToNext();
+			exercises[i] = c.getString(0);
+		}
 
-        return e.toArray(new String[e.size()]);
+        return exercises;
     }
 
-    public static String convertArrayToString(String[] array){
-        String str = "";
-        for (int i = 0;i<array.length; i++) {
-            str = str+array[i];
-            // Do not append comma at the end of last element
-            if(i<array.length-1){
-                str = str+strSeparator;
-            }
-        }
-
-        return str;
-    }
-
-    public static String[] convertStringToArray(String str){
-        String[] arr = str.split(strSeparator);
-        return arr;
-    }
 }
--- app/src/main/java/tracker/workout/workouttracker/LogWorkoutActivity.java
+++ app/src/main/java/tracker/workout/workouttracker/LogWorkoutActivity.java
@@ -1,8 +1,9 @@
 package tracker.workout.workouttracker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,39 +17,68 @@ public class LogWorkoutActivity extends AppCompatActivity {
     private ListView gridView;
     private Button emptyButton;
     private TextView emptyText;
-    private DatabaseHelper myDb;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+
         setContentView(R.layout.activity_log_workout);
         gridView = (ListView) findViewById(R.id.workoutListView);
-        myDb = new DatabaseHelper(this);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.list_item, myDb.getWorkoutNames());
-        gridView.setAdapter(adapter);
 
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String category = parent.getItemAtPosition(position).toString();
-                String[] exercises = myDb.getWorkoutExercises(category);
-                Intent intent = new Intent(LogWorkoutActivity.this, LogThisWorkoutActivity.class);
-                intent.putExtra("exercises", exercises);
-                startActivity(intent);
-            }
-        });
+		new PopulateGridViewTask().execute();
 
-        emptyButton=(Button)findViewById(R.id.emptyCreateWorkoutButton);
+        emptyButton = (Button) findViewById(R.id.emptyCreateWorkoutButton);
         gridView.setEmptyView(emptyButton);
-        emptyText=(TextView)findViewById(R.id.emptyText);
+        emptyText = (TextView) findViewById(R.id.emptyText);
         gridView.setEmptyView(emptyText);
 
         emptyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent addActivity = new Intent(LogWorkoutActivity.this, CreateWorkoutActivity.class);
+                Intent addActivity = new Intent(getApplicationContext(), CreateWorkoutActivity.class);
                 startActivity(addActivity);
             }
         });
     }
+
+	private class LogThisWorkoutTask extends AsyncTask<String, Void, String[]> {
+		@Override
+		protected String[] doInBackground(String... strings) {
+			return databaseHelper.getWorkoutExercises(strings[0]);
+		}
+
+		@Override
+		protected void onPostExecute(String[] exercises) {
+			Intent intent = new Intent(getApplicationContext(), LogThisWorkoutActivity.class);
+			intent.putExtra("exercises", exercises);
+			startActivity(intent);
+		}
+	}
+
+	private class PopulateGridViewTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... voids) {
+			final String[] workoutNames = databaseHelper.getWorkoutNames();
+
+			// Can't access view hierarchy on a different thread so run on UI thread after querying db.
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					gridView.setAdapter(new ArrayAdapter(getApplicationContext(), R.layout.list_item, workoutNames));
+
+					gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+						@Override
+						public void onItemClick(final AdapterView<?> parent, View view, final int position, long id) {
+							new LogThisWorkoutTask().execute(parent.getItemAtPosition(position).toString());
+						}
+					});
+				}
+			});
+			return null;
+		}
+	}
+
 }
